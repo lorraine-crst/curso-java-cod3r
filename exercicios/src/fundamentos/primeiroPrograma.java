de50ccecf4089fbc5c81e6627b294d20fd02eb69
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-06-28 23:54:59 -0300

    Create primeiroPrograma.java

---
+++ exercicios/src/fundamentos/primeiroPrograma.java
@@ -0,0 +1,7 @@
+package fundamentos;
+
+public class primeiroPrograma {
+	public static void main(String[] args) {
+		System.out.println("Hello Word em Java, parabéns"); /*imprime uma linha na saida do sistema*/
+	}
+}
