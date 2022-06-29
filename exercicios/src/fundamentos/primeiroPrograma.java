f986c41a02313fcdacea57fa7f28842aa204f41b
Lorraine Cristina <89432049+lorraine-crst@users.noreply.github.com>
2022-06-29 00:10:02 -0300

    Update primeiroPrograma.java

--- exercicios/src/fundamentos/primeiroPrograma.java
+++ exercicios/src/fundamentos/primeiroPrograma.java
@@ -2,6 +2,9 @@ package fundamentos;
 
 public class primeiroPrograma {
 	public static void main(String[] args) {
-		System.out.println("Hello Word em Java, parabéns"); /*imprime uma linha na saida do sistema*/
+		System.out.println("Hello Word "
+				+ "em Java, parabéns"); /*imprime uma linha na saida do console*/
+		System.out.println("Teste para ver se posso dar console sem reescrever novamente");
+		System.out.println("Sim, posso reaproveitar o codigo para reescrever no console"); 
 	}
 }
